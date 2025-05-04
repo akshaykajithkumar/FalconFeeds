f33ad010a211f78c75640eb2179db42d917ef8bc
akshay <akshaykajithkumar@gmail.com>
2025-05-05 03:24:47 +0530

    added screen shot

--- normalizer/internal/shared/mongo.go
+++ normalizer/internal/shared/mongo.go
@@ -9,16 +9,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// func NewMongoCollection() (*mongo.Client, *mongo.Collection) {
-// 	ctx := context.Background()
-// 	clientOpts := options.Client().ApplyURI("mongodb://mongo:27017")
-// 	client, err := mongo.Connect(ctx, clientOpts)
-// 	if err != nil {
-// 		log.Fatalf("MongoDB connect error: %v", err)
-// 	}
-
-//		return client, client.Database("falconfeeds").Collection("normalized-indicators")
-//	}
 func NewMongoCollection() (*mongo.Client, *mongo.Collection) {
 	ctx := context.Background()
 
@@ -40,36 +30,3 @@ func NewMongoCollection() (*mongo.Client, *mongo.Collection) {
 
 	return client, client.Database("falconfeeds").Collection("normalized-indicators")
 }
-
-// package shared
-
-// import (
-// 	"context"
-// 	"log"
-// 	"time"
-
-// 	"go.mongodb.org/mongo-driver/mongo"
-// 	"go.mongodb.org/mongo-driver/mongo/options"
-// )
-
-// func NewMongoCollection() (*mongo.Client, *mongo.Collection) {
-// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-// 	defer cancel()
-
-// 	client, err := mongo.Connect(ctx, options.Client().
-// 		ApplyURI("mongodb://localhost:27017").
-// 		SetServerSelectionTimeout(5*time.Second))
-// 	if err != nil {
-// 		log.Fatalf("Failed to connect to MongoDB: %v", err)
-// 	}
-
-// 	// Verify connection
-// 	ctxPing, cancelPing := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancelPing()
-// 	if err = client.Ping(ctxPing, nil); err != nil {
-// 		log.Fatalf("MongoDB ping failed: %v", err)
-// 	}
-
-// 	collection := client.Database("test").Collection("stix")
-// 	return client, collection
-// }
